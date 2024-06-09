21dc951a31d5175072787b14b828d0048595ab47
kubamichalak <jakub.michalak98@wp.pl>
2024-06-09 21:18:10 +0200

    DB-1 #init-app

---
+++ src/app/app.config.ts
@@ -0,0 +1,30 @@
+import {
+	ApplicationConfig,
+	isDevMode,
+	provideExperimentalZonelessChangeDetection,
+	provideZoneChangeDetection
+} from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+
+import routes from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+export const appConfig: ApplicationConfig = {
+	providers: [
+		provideExperimentalZonelessChangeDetection(),
+		provideRouter(routes, withViewTransitions()),
+		provideHttpClient(withFetch()),
+		provideStore(),
+		provideStoreDevtools({
+			maxAge: 25,
+			logOnly: !isDevMode(),
+			autoPause: true,
+			trace: false,
+			traceLimit: 75
+		}),
+		provideAnimationsAsync()
+	]
+};

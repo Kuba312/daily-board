a4edd76a23e0883450a04a137df836303044a451
kubamichalak <jakub.michalak98@wp.pl>
2024-08-26 19:27:51 +0200

    PA-11 init backend

---
+++ src/main/java/com/daily_board/daily_board/DailyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.daily_board.daily_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyBoardApplication.class, args);
+	}
+
+}
